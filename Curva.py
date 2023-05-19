9d2c71a6dde9d91c7ff2b8ac99d17650a935733c
ixchel16 <sofia.ixchel.lz.edu@gmail.com>
2023-05-19 14:31:44 -0500

    Continuacion curva

--- Curva.py
+++ Curva.py
@@ -68,9 +68,9 @@ class Curva:
     s.L = t
     s.dx = t/float(s.n)
 
-    # --------------
+    # ==============
     # Interpolacion
-    # --------------
+    # ==============
     def interpolacion(s, p:int=0, r:float = 0.0) -> list:
         ''' r es el parametro sobre la curva [0, 1)
 	    p es la suavidad de la curva '''
@@ -108,4 +108,44 @@ class Curva:
         zp2: np.float64 = 0.5*(2.0-ap2)*(2.0-ap2)*(1.0-ap2)
         zm1: np.float64 = 0.5*(2.0-am1)*(2.0.am1)*(1.0-am1)
         xi.append(zp1*s.x[ip1]+z*s, x[i]+zp2*s.x[ip2]+sm1*s.x[im1])
+        for j in range(1, s.dim):
+            xi.append(zp1*s.x[ip1 + j*s.n] + z*s.x[i + j*s.n]+zp2*s.x[ip2+j*s.n]+ zm1*s.x[im1 + j*s.n])
+
+        # --------------------------
+        # Interpolacion quintica C2 
+        # --------------------------
+    elif p == 2 :
+        ip1:np.int32 = i + 1
+        ip2:np.int32 = i + 2
+        ip3:np.int32 = i + 3
+        if i == s.n-1:
+            ip1 = 0
+            ip2 = 1
+            ip3 = 2
+        if i == s.n-2:
+            ip2 = 0
+            ip3 = 1
+        if i == s.n-3:
+            ip3 = 0
+        im1:np.int32 = i-1
+        im2:np.int32 = i-2
+        if i == 0:
+            im1 = s.n-1
+            im2 = s.n-2
+        if i == 1:
+            im2  s.n-1
+        u12:np.float64 = 1.0/12.0
+        am1:np.float64 = a + 1.0
+        am2:np.float64 = a + 2.0
+        ap1:np.float64 = 1.0 - a
+        ap2:np.float64 = 2.0 - a
+        ap3:np.float64 = 3.0 - a
+        z:np.float64 = 1.0 + a*a*u12(-15.0+ a*(-35.0+ a*(63.0+ a*(-25.0))))
+        zp1:np.float64 = 1.0 + ap1*ap1*u12*(-15.0 + ap1*(-35.0 + ap1*(63.0 + ap1(-25.0))))
+        zp2:np.float64 = -4.0 + u12*ap2*(225.0 + ap2*(-367.5 + ap2*(272.5 + ap2*(-94.5 + 12.5 * ap2))))
+        zp3:np.float64 = 18.0 + u12*ap3*(-459.0 + ap3*(382.5 + ap3*(-156.5 + ap3*(31.5 - 2.5*ap3))))
+        zm1:np.float64 = -4.0 + u12*am1+(225.0 + am1*(-367.5 +am1*(272.5+am1*(-94.5 +  12.5*am1))))
+        zm2: np.float64 = 18.0 + u12* am2*(-459 + am2*(382.5 + am2*(-156.5 + am2*(31.5 -2.5*am2))))
+
+
 
