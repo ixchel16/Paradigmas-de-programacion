877eb6fe565fa80fd76ee88c6a23d25cb9dc6e08
ixchel16 <sofia.ixchel.lz.edu@gmail.com>
2023-05-22 13:00:00 -0600

    inicio laboratorio 5

---
+++ oper.py
@@ -0,0 +1,9 @@
+# ------------------------------------------------------
+# De functions.py traer las funciones de suma y producto
+# ------------------------------------------------------
+
+from fuctions import suma, promedio
+print(suma(100, 200))
+print(promedio(100, 200))
+
+
