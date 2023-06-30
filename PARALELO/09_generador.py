1b9c927b9b7669a163c8afa4ced59c803115e606
ixchel16 <sofia.ixchel.lz.edu@gmail.com>
2023-06-30 09:51:03 -0600

    Finalización y corrección de códigos

--- PARALELO/09_generador.py
+++ PARALELO/09_generador.py
@@ -2,12 +2,14 @@ import random
 
 N:int = 10
 
-def generador(N:float) -> None:
-    semilla:float = random.uniform(-1, 1)
-        print("La semilla es: ", semilla)
-        random.seed(semilla)
-        for i in range(N):
-            x:float = random.uniform(-1, 1)
-            y:float = random.uniform(-1, 1)
-            print("x = ", x, "\t y = ", y)            
+def generador(N:float):
+	semilla:float = random.uniform(-1, 1)
+	print("La semilla es: ", semilla)
+	random.seed(semilla)
+	for i in range(N):
+		x:float = random.uniform(-1, 1)
+		y:float = random.uniform(-1, 1)
+		print("X = ", x, "\t Y = ", y)            
+
 generador(N)
+
--- PARALELO/10_generador_paralelo.py
+++ PARALELO/10_generador_paralelo.py
@@ -6,12 +6,12 @@ N:int = 10
     
 def generador(N:float) -> None:
     semilla:float = random.uniform(-1, 1)
-        print("La semilla es: ", semilla)
-        random.seed(semilla)
-        for i in range(N):
-            x:float = random.uniform(-1, 1)
-            y:float = random.uniform(-1, 1)
-            print("x = ", x, "\t y = ", y)   
+    print("La semilla es: ", semilla)
+    random.seed(semilla)
+    for i in range(N):
+        x:float = random.uniform(-1, 1)
+        y:float = random.uniform(-1, 1)
+        print("x = ", x, "\t y = ", y)   
             
 procesos = []
 cpus = os.cpu_count()
